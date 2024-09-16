8a44c53193335416208fe999124dc5b13346951a
ThatDudeGuy <waffles1999@icloud.com>
2024-09-16 18:46:45 -0400

    Added Week 4 Extra Credit

---
+++ Week4/lab_7_Starter_part1.py
@@ -0,0 +1,28 @@
+"""
+
+The Albright Public Safety need some help with checking if people
+are speeding. 
+
+
+1. You will ensure that you have left 3 comments at the top of your file
+    # Your Name
+    # Date
+    # brief description of what this file does
+
+2. For this program, you must prompt the user to enter the speed limit
+    and the speed at which they were driving.
+
+3. Once you have collected your data, you will check if the user was in fact
+    speeding or not using conditional statements. If they are speeding, print
+    a message that says they are speeding, else if they are excessivley speeding,
+    show a message that says so, otherwise, show a message that lets
+    them know they are safe. Your choice for what your messages say as long
+    as they make sense. 
+
+    *** Excessive speeding is considered 30mph over the posted speed limit ***
+
+4. After completing above 3 steps, remove all comments in this file that are
+    not your own and do not forget to rename this file to speedTrap.py
+
+"""
+
---
+++ Week4/lab_7_Starter_part2.py
@@ -0,0 +1,28 @@
+"""
+
+Canvas's grade checker is broken and you need to fix it! 
+
+
+1. You will ensure that you have left 3 comments at the top of your file
+    # Your Name
+    # Date
+    # brief description of what this file does
+
+2. For this program, you must prompt the user to enter their final grade score
+
+3. Once you have collected your data, you will use ONE multi-branch conditional
+    statement to print a message that reflects the letter grade that they have
+    received. Additionaly, if the user enters a value above 100 and or below 0,
+    show a message that an error has occured
+
+    *** Use the following grade scale ***
+                100-90 = A
+                89-80  = B
+                79-70  = C
+                69-60  = D
+                59-0   = F
+
+4. After completing above 3 steps, remove all comments in this file that are
+    not your own and do not forget to rename this file to studentGrade.py
+
+"""
